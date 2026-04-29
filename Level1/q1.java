2b0a3e80bc2f5fe6bb939abac2dc15b22780e467
vidhi-me-space <vidhi.me.space@gmail.com>
2026-04-29 10:40:34 +0530

    coded

---
+++ Level1/q1.java
@@ -0,0 +1,13 @@
+public class q1 {
+    public static void main(String[] args) {
+        // Initialize the given values
+        int birthYear = 2000;
+        int currentYear = 2024;
+        
+        // Calculate the age
+        int age = currentYear - birthYear;
+        
+        // Print the output
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
