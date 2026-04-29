e9b60ab667edbaf56d9a8be957f519fa84765135
vidhi-me-space <vidhi.me.space@gmail.com>
2026-04-29 10:46:15 +0530

    coded

---
+++ Level1/q3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter number1: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Enter number2: ");
+        int number2 = sc.nextInt();
+
+        System.out.print("Enter number3: ");
+        int number3 = sc.nextInt();
+
+        // Check largest
+        boolean firstLargest = (number1 > number2) && (number1 > number3);
+        boolean secondLargest = (number2 > number1) && (number2 > number3);
+        boolean thirdLargest = (number3 > number1) && (number3 > number2);
+
+        // Output
+        System.out.println("Is the first number the largest? " + firstLargest);
+        System.out.println("Is the second number the largest? " + secondLargest);
+        System.out.println("Is the third number the largest? " + thirdLargest);
+
+        sc.close();
+    }
+}
