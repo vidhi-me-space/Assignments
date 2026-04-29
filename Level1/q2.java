9e8c28ea4ce742a53b04fc5f2b429645b7729db3
vidhi-me-space <vidhi.me.space@gmail.com>
2026-04-29 10:40:35 +0530

    coded

---
+++ Level1/q2.java
@@ -0,0 +1,14 @@
+public class q2 {
+    public static void main(String[] args) {
+        // Initialize the marks
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        
+        // Calculate the average
+        double average = (maths + physics + chemistry) / 3.0;
+        
+        // Print the output
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
